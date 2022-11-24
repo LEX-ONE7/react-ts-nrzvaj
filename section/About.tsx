6ca38aea7636cf8f26b4fbbe466f653926c68664
LEX-ONE7 <118214894+LEX-ONE7@users.noreply.github.com>
2022-11-24 09:56:28 -1000

    1 file modified

--- section/About.tsx
+++ section/About.tsx
@@ -3,31 +3,31 @@ import * as React from 'react';
 const About = () => {
   return (
     <section id="about">
-      <div class="container my-5 py-5">
-        <div class="row mb-5">
-          <div class="col-12">
-            <h1 class="fw-bold text-center">En savoir plus</h1>
+      <div className="container my-5 py-5">
+        <div className="row mb-5">
+          <div className="col-12">
+            <h1 className="fw-bold text-center">En savoir plus</h1>
 
             <hr />
           </div>
         </div>
 
-        <div class="row">
-          <div class="col-md-4 img-profil overflow-hidden">
+        <div className="row">
+          <div className="col-md-4 img-profil overflow-hidden">
             <img
-              class="img-fluid"
+              className="img-fluid"
               src="https://picsum.photos/id/119/1000/1300.jpg"
               alt=""
               height="500px"
             />
           </div>
 
-          <div class="col-md-8">
-            <h1 class="fw-bold mt-4">TAURUA Alexandre</h1>
+          <div className="col-md-8">
+            <h1 className="fw-bold mt-4">TAURUA Alexandre</h1>
 
-            <h3 class="text-info mb-4">Développeur WEB</h3>
+            <h3 className="text-info mb-4">Développeur WEB</h3>
 
-            <p class="lead mb-4">
+            <p className="lead mb-4">
               Passionné par la réalisation et le développement de projets web,
               je me suis tout naturellement tourné vers une carrière de
               développeur web. Aujourd'hui je souhaite intégrer une entreprise
@@ -35,12 +35,12 @@ const About = () => {
               en tant que développeur web.
             </p>
 
-            <div class="buttons">
-              <button class="btn btn-outline-primary px-5 py-2">
+            <div className="buttons">
+              <button className="btn btn-outline-primary px-5 py-2">
                 Télécharger CV
               </button>
 
-              <button class="btn btn-info text-white color-black px-4 py-2">
+              <button className="btn btn-info text-white color-black px-4 py-2">
                 Contact
               </button>
             </div>

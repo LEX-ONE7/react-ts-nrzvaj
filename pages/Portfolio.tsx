fadd2e3ca85f47d602f75e95c5b02e74204c1ed7
LEX-ONE7 <118214894+LEX-ONE7@users.noreply.github.com>
2022-11-24 09:48:34 -1000

    3 files modified, 4 added, 4 deleted

--- pages/Portfolio.tsx
+++ pages/Portfolio.tsx
@@ -1,9 +1,17 @@
 import * as React from 'react';
+import Contact from '../section/Contact';
+import Header from '../section/Header';
+import Navbar from '../section/Navbar';
+import Services from '../section/Services';
+import Skills from '../section/Skills';
 
 const Portfolio = () => (
   <div>
-    <h2>Mon Portfolio</h2>
-    
+    <Navbar />
+    <Header />
+    <Skills />
+    <Services />
+    <Contact />
   </div>
 );
 
